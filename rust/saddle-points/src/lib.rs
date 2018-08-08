a7ceea004b977aa0bbc46f5ea9bb2ecbd2fca5d6
Dan Nguyen <pedantic@gmail.com>
2018-08-08 10:18:20 -0500

    Change rust saddle-points impl based on feedback

--- rust/saddle-points/src/lib.rs
+++ rust/saddle-points/src/lib.rs
@@ -1,23 +1,16 @@
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    let mut points = Vec::new();
+    let h = input.len();
+    let w = input[0].len();
 
-    for (r, row) in input.iter().enumerate() {
-        for c in 0..row.len() {
-            let cell = input[r][c];
-            let col: Vec<u64> = input.iter().map(|v| v[c]).collect();
+    (0..h)
+        .flat_map(|x| (0..w).map(|y| (x, y)).collect::<Vec<(usize, usize)>>())
+        .filter(|(r, c)| {
+            let cell = input[*r][*c];
+            let col: Vec<u64> = input.iter().map(|v| v[*c]).collect();
 
-            match row.iter().max() {
-                Some(rmax) => match col.iter().min() {
-                    Some(cmin) => {
-                        if cell >= *rmax && cell <= *cmin {
-                            points.push((r, c));
-                        }
-                    },
-                    None => {},
-                },
-                None => {},
-            }
-        }
-    }
-    points
+            let rmax = input[*r].iter().max().unwrap();
+            let cmin = col.iter().min().unwrap();
+            cell >= *rmax && cell <= *cmin
+        })
+        .collect::<Vec<(usize, usize)>>()
 }
