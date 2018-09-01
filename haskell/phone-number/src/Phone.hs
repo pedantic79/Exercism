b9a06cd602249e463eb92d737e70e737ddd780cf
Dan Nguyen <pedantic@gmail.com>
2018-08-31 23:29:14 -0500

    Add phone number

--- haskell/phone-number/src/Phone.hs
+++ haskell/phone-number/src/Phone.hs
@@ -1,4 +1,23 @@
 module Phone (number) where
 
+import Data.Char (isDigit)
+
 number :: String -> Maybe String
-number xs = error "You need to implement this function."
+number = (valid =<<) . cleanup
+
+valid :: String -> Maybe String
+valid input
+    | length input /= 10       = Nothing
+    | head input `elem` "01"   = Nothing
+    | (input !! 3) `elem` "01" = Nothing
+    | otherwise                = Just input
+
+cleanup :: String -> Maybe String
+cleanup input
+    | len == 10            = Just digits
+    | len == 11 && headOne = Just $ tail digits
+    | otherwise            = Nothing
+    where
+        digits  = filter isDigit input
+        len     = length digits
+        headOne = head digits == '1'
