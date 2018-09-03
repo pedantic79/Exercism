db412c30c12ac7feb704af4694bfb3777c609a1f
Dan Nguyen <pedantic@gmail.com>
2018-09-03 11:48:26 -0500

    Add anagram rust

---
+++ rust/anagram/benches/benchmark.rs
@@ -0,0 +1,39 @@
+#[macro_use]
+extern crate criterion;
+extern crate anagram;
+
+use anagram::*;
+use criterion::Criterion;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let word = "allergy";
+
+    let inputs = [
+        "gallery",
+        "ballerina",
+        "regally",
+        "clergy",
+        "largely",
+        "leading",
+    ];
+
+    c.bench_function("map allergy", move |b| b.iter(|| anagrams_map(&word, &inputs)));
+    c.bench_function("sort allergy", move |b| b.iter(|| anagrams_sort(&word, &inputs)));
+}
+
+fn adeinr(c: &mut Criterion) {
+    let word = "adeinr";
+    let inputs = [
+        "adrien", "adrine", "ainder", "anderi", "andire", "andrei", "andrie", "ardine", "dairen",
+        "daneri", "danier", "darein", "darien", "darine", "dearin", "deiran", "denair", "denari",
+        "denira", "derain", "derian", "derina", "dierna", "draine", "drenai", "drenia", "edrian",
+        "erandi", "eridan", "erinda", "idanre", "indear", "indera", "naderi", "nadier", "nadire",
+        "nareid", "neidar", "neriad", "nerida", "niedra", "radien", "raiden", "rained", "randie",
+        "rdeina", "readin", "redian", "redina", "renida", "riande", "rienda",
+    ];
+    c.bench_function("map adeinr", move |b| b.iter(|| anagrams_map(&word, &inputs)));
+    c.bench_function("sort adeinr", move |b| b.iter(|| anagrams_sort(&word, &inputs)));
+}
+
+criterion_group!(benches, criterion_benchmark, adeinr);
+criterion_main!(benches);
---
+++ rust/anagram/src/lib.rs
@@ -0,0 +1,50 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+fn sort_string(s: &String) -> String {
+    let mut v = s.chars().collect::<Vec<char>>();
+    v.sort();
+    v.iter().collect()
+}
+
+fn get_frequency(s: &String) -> HashMap<char, usize> {
+    let mut m = HashMap::new();
+    for c in s.chars() {
+        *m.entry(c).or_insert(1) += 1;
+    }
+    m
+}
+
+#[inline]
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    // Sort is faster
+    anagrams_sort(word, possible_anagrams)
+}
+
+#[inline]
+pub fn anagrams_sort<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    anagrams_fn(word, possible_anagrams, sort_string)
+}
+
+#[inline]
+pub fn anagrams_map<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    anagrams_fn(word, possible_anagrams, get_frequency)
+}
+
+pub fn anagrams_fn<'a, T: Eq>(
+    word: &str,
+    possible_anagrams: &[&'a str],
+    f: fn(&String) -> T,
+) -> HashSet<&'a str> {
+    let lower = word.to_lowercase();
+    let fnstr = f(&lower);
+
+    possible_anagrams
+        .iter()
+        .filter(|anagram| {
+            let l = anagram.to_lowercase();
+            lower != l && fnstr == f(&l)
+        })
+        .cloned()
+        .collect()
+}
