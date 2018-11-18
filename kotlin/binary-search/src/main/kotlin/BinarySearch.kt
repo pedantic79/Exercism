767378d689d2f1fd41d373e31995ee4f455a0c17
Dan Nguyen <pedantic@gmail.com>
2018-11-18 12:56:33 -0600

    Fix kotlin/binary-search based on feedback

--- kotlin/binary-search/src/main/kotlin/BinarySearch.kt
+++ kotlin/binary-search/src/main/kotlin/BinarySearch.kt
@@ -1,19 +1,14 @@
 object BinarySearch {
-    fun <T> search(list: List<T>, n: T): Int where T : Comparable<T> = searchPos(list, n, 0)
-}
-
-private fun <T> searchPos(list: List<T>, n: T, offset: Int): Int
-        where T : Comparable<T> =
-        when {
-            list.isEmpty() -> -1
-            list[0] == n   -> offset
-            list.size == 1 -> -1 // This will be true if a size 1 list does not have our search value
-            else -> {
-                val midpoint = list.size / 2
-
-                if (n < list[midpoint])
-                    searchPos(list.subList(0, midpoint), n, offset)
-                else
-                    searchPos(list.subList(midpoint, list.size), n, offset + midpoint)
+    tailrec fun <T> search(list: List<T>, n: T, start: Int = 0, end: Int = list.size): Int where T : Comparable<T> =
+            (end - start).let { size ->
+                if (size == 0) -1
+                else {
+                    val midpoint = start + size / 2
+                    when {
+                        n == list[midpoint] -> midpoint
+                        n < list[midpoint] -> search(list, n, start, midpoint)
+                        else -> search(list, n, midpoint + 1, end)
+                    }
+                }
             }
-        }
+}
