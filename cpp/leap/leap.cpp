012c3aff388114a603ab4dd1d97da90b5f3e2847
Dan Nguyen <pedantic@gmail.com>
2018-11-18 21:41:26 -0600

    Fix cpp/leap

--- cpp/leap/leap.cpp
+++ cpp/leap/leap.cpp
@@ -4,6 +4,6 @@ namespace leap
 {
     bool is_leap_year(int year)
     {
-        return (year % 400 == 0 && year % 100 != 0) || (year % 4 == 0);
+        return (year % 400 == 0 || year % 100 != 0) && (year % 4 == 0);
     }
 }
