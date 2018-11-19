c9efb6347f125bc8f9751b67145a53fea27ad463
Dan Nguyen <pedantic@gmail.com>
2018-11-18 22:48:37 -0600

    Add cpp/binary

---
+++ cpp/binary/binary.cpp
@@ -0,0 +1,17 @@
+#include "binary.h"
+
+namespace binary {
+    int convert(std::string_view binary_number) {
+        int result = 0;
+
+        for (const auto& c : binary_number) {
+            result <<= 2;
+            if (c == '1') {
+                result |= 1;
+            } else if (c != '0') {
+                return 0;
+            }
+        }
+        return result;
+    }
+}
---
+++ cpp/binary/binary.h
@@ -0,0 +1,9 @@
+#ifndef BINARY_H
+#define BINARY_H
+#include <string>
+
+namespace binary {
+    int convert(std::string_view binary_number);
+}
+
+#endif
