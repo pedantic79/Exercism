c9acc9494f62976d2806a0861b42c4ad82975d59
Dan Nguyen <pedantic@gmail.com>
2018-11-19 21:41:36 -0600

    Add rust/raindrops

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,17 +1,22 @@
-pub fn raindrops(n: usize) -> String {
-    let ppp = vec![3, 5, 7]
-        .iter()
-        .filter(|i| n % *i == 0)
-        .map(|i| match *i {
-            3 => "Pling",
-            5 => "Plang",
-            _ => "Plong",
-        })
-        .collect::<String>();
+#[macro_use]
+extern crate indexmap;
 
-    if ppp == "" {
+pub fn raindrops(n: usize) -> String {
+    let divisible = indexmap!{
+        3 => "Pling",
+        5 => "Plang",
+        7 => "Plong",
+    };
+
+    let v: String = divisible
+        .iter()
+        .filter(|(&d, _)| n % d == 0)
+        .map(|(_, &value)| value)
+        .collect();
+
+    if v == "" {
         n.to_string()
     } else {
-        ppp
+        v
     }
 }
