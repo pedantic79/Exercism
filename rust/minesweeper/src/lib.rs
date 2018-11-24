dad82fb51e22479f2cd65c673ed115fc2e22a210
Dan Nguyen <pedantic@gmail.com>
2018-11-24 09:09:28 -0600

    Rework rust/minesweeper

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -18,13 +18,19 @@ impl Into<Vec<String>> for Board {
 }
 
 impl Board {
-    fn annotate(&mut self) {
-        self.places = (0..self.places.len())
-            .map(|x| {
-                (0..self.places[x].len())
-                    .map(|y| self.count_mines(x, y))
+    fn annotate(&self) -> Board {
+        let v = self
+            .places
+            .iter()
+            .enumerate()
+            .map(|(x, row)| {
+                row.iter()
+                    .enumerate()
+                    .map(|(y, _)| self.count_mines(x, y))
                     .collect()
             }).collect();
+
+        Board { places: v }
     }
 
     fn count_mines(&self, r: usize, c: usize) -> char {
@@ -47,15 +53,8 @@ impl Board {
     }
 
     fn neighbors(&self, r: usize, c: usize) -> Vec<(usize, usize)> {
-        let row_max = self.places.len() - 1;
-        let col_max = self.places[0].len() - 1;
-
-        let min_r = if r < 1 { 0 } else { r - 1 };
-        let max_r = if r == row_max { row_max } else { r + 1 };
-
-        let min_c = if c < 1 { 0 } else { c - 1 };
-        let max_c = if c == col_max { col_max } else { c + 1 };
-
+        let (min_r, max_r) = get_range(r, self.places.len());
+        let (min_c, max_c) = get_range(c, self.places[r].len());
         let mut v = Vec::new();
 
         for row in min_r..=max_r {
@@ -69,7 +68,11 @@ impl Board {
 }
 
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-    let mut b = Board::from(minefield);
-    b.annotate();
-    b.into()
+    Board::from(minefield).annotate().into()
+}
+
+fn get_range(n: usize, n_max: usize) -> (usize, usize) {
+    let min = if n == 0 { 0 } else { n - 1 };
+    let max = if n == n_max - 1 { n_max - 1 } else { n + 1 };
+    (min, max)
 }
