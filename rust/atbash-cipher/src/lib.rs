111842725cc68f95a9e3d53d051d9661997fb03d
Dan Nguyen <pedantic@gmail.com>
2018-12-09 12:16:08 -0600

    Fix rust/atbash-cipher

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -1,33 +1,31 @@
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    plain
-        .chars()
-        .filter(|x| x.is_ascii())
-        .filter(|x| x.is_alphanumeric())
-        .map(|x| x.to_ascii_lowercase())
-        .map(|x| at_bash(x))
-        .collect::<Vec<char>>()
-        .chunks(5)
-        .map(|group| group.iter().collect::<String>())
-        .collect::<Vec<String>>()
-        .join(" ")
+        plain.chars()
+                .filter(|x| x.is_ascii())
+                .filter(|x| x.is_alphanumeric())
+                .map(|x| x.to_ascii_lowercase())
+                .map(at_bash)
+                .collect::<Vec<char>>()
+                .chunks(5)
+                .map(|group| group.iter().collect::<String>())
+                .collect::<Vec<String>>()
+                .join(" ")
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    cipher
-        .chars()
-        .filter(|x| x.is_alphanumeric())
-        .map(|x| at_bash(x))
-        .collect::<String>()
+        cipher.chars()
+                .filter(|x| x.is_alphanumeric())
+                .map(at_bash)
+                .collect::<String>()
 }
 
 pub fn at_bash(ch: char) -> char {
-    if ch.is_numeric() {
-        ch
-    } else {
-        let a = 'a' as u8;
-        let z = 'z' as u8;
-        (a + z - ch as u8) as char
-    }
+        if ch.is_numeric() {
+                ch
+        } else {
+                let a = b'a';
+                let z = b'z';
+                (a + z - ch as u8) as char
+        }
 }
