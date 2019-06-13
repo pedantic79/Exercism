a1f0ca2509605dcf751ce6aecd5888e473330c7f
Dan Nguyen <pedantic@gmail.com>
2019-06-12 20:09:59 -0500

    Add javascript/resistor-color

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,8 @@
+export const colorCode = (color) => {
+  return COLORS.indexOf(color);
+};
+
+export const COLORS = [
+  'black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet',
+  'grey', 'white'
+];
