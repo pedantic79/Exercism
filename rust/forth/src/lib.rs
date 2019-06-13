3db002531a7f679a72e9ebfd927d3210b0a27d2b
Dan Nguyen <pedantic@gmail.com>
2019-06-12 20:09:59 -0500

    Add javascript/resistor-color

--- rust/forth/src/lib.rs
+++ rust/forth/src/lib.rs
@@ -1,7 +1,17 @@
+mod stack;
+mod token;
+
+use std::collections::HashMap;
+
 pub type Value = i32;
 pub type ForthResult = Result<(), Error>;
+use stack::Stack;
+use token::Token;
 
-pub struct Forth;
+pub struct Forth {
+    stack: Stack,
+    words: HashMap<Token, Vec<Token>>,
+}
 
 #[derive(Debug, PartialEq)]
 pub enum Error {
@@ -11,16 +21,54 @@ pub enum Error {
     InvalidWord,
 }
 
+impl Default for Forth {
+    fn default() -> Self {
+        Self {
+            stack: Stack::new(),
+            words: HashMap::new(),
+        }
+    }
+}
+
 impl Forth {
-    pub fn new() -> Forth {
-        unimplemented!()
+    pub fn new() -> Self {
+        Self::default()
     }
 
     pub fn stack(&self) -> Vec<Value> {
-        unimplemented!()
+        self.stack.get()
+    }
+
+    fn add_func(&mut self, tokens: Vec<Token>) -> ForthResult {
+        let len = tokens.len();
+
+        if len > 3 && tokens[0] == Token::WordStart && tokens[len - 1] == Token::WordEnd {
+            let name = &tokens[1];
+            if let Token::Num(_) = name {
+                Err(Error::InvalidWord)
+            } else if *name == Token::WordStart || *name == Token::WordEnd  {
+                Err(Error::InvalidWord)
+            } else {
+                self.words.insert(name.clone(), Vec::from(&tokens[2..len-1]));
+                Ok(())
+            }
+        } else {
+            Err(Error::InvalidWord)
+        }
+
     }
 
     pub fn eval(&mut self, input: &str) -> ForthResult {
-        unimplemented!("result of evaluating '{}'", input)
+        let tokens: Vec<Token> = input
+            .split_whitespace()
+            .map(|s| s.to_uppercase().parse::<Token>().unwrap())
+            .collect();
+
+        if tokens.len() > 1 && tokens[0] == Token::WordStart {
+            self.add_func(tokens)
+        } else {
+            Err(Error::UnknownWord)
+        }
+
     }
 }
