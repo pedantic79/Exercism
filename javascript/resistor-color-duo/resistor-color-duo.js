dc1570249aff5b804b1059720bd42c5a936b702c
Dan Nguyen <pedantic@gmail.com>
2019-06-13 22:25:07 -0500

    Add javascript/leap

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,10 +1,10 @@
 export const value = (colors) => {
+  const COLORS = [
+    'black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet',
+    'grey', 'white'
+  ];
+
   return parseInt(colors.reduce((accumulator, value) => {
     return accumulator + COLORS.indexOf(value).toString();
   }, ''));
 };
-
-const COLORS = [
-  'black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet',
-  'grey', 'white'
-];
