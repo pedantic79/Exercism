807febf9f4e26f8dbe4b2438d036939ead50d9f0
Dan Nguyen <pedantic@gmail.com>
2019-06-16 02:19:23 -0500

    Rework javascript/roman-numerals

--- javascript/roman-numerals/roman-numerals.js
+++ javascript/roman-numerals/roman-numerals.js
@@ -2,9 +2,9 @@ export const toRoman = (arabicNumber) => {
   let num = arabicNumber;
   let roman = '';
 
-  for (const divisor of ROMAN_NUMERALS.keys()) {
+  for (const divisor of Object.keys(ROMAN_NUMERALS).sort((a, b) => b - a)) {
     while (parseInt(num / divisor) > 0) {
-      roman += ROMAN_NUMERALS.get(divisor);
+      roman += ROMAN_NUMERALS[divisor];
       num -= divisor;
     }
   }
@@ -12,7 +12,18 @@ export const toRoman = (arabicNumber) => {
   return roman;
 };
 
-const ROMAN_NUMERALS = new Map([
-  [1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'], [100, 'C'], [90, 'XC'],
-  [50, 'L'], [40, 'XL'], [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']
-]);
+const ROMAN_NUMERALS = {
+  1000: 'M',
+  900: 'CM',
+  500: 'D',
+  400: 'CD',
+  100: 'C',
+  90: 'XC',
+  50: 'L',
+  40: 'XL',
+  10: 'X',
+  9: 'IX',
+  5: 'V',
+  4: 'IV',
+  1: 'I'
+};
