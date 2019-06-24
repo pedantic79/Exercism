ecf12f8179e717130c357e649296b3576b883098
Dan Nguyen <pedantic@gmail.com>
2019-06-23 22:22:54 -0500

    Add rust/pangram

---
+++ rust/pangram/src/lib.rs
@@ -0,0 +1,12 @@
+use std::collections::HashSet;
+
+/// Determine whether a sentence is a pangram.
+pub fn is_pangram(sentence: &str) -> bool {
+    let s = sentence
+        .chars()
+        .filter(|c| c.is_ascii_alphabetic())
+        .map(|c| c.to_ascii_lowercase())
+        .collect::<HashSet<_>>();
+
+    s.len() == 26
+}
