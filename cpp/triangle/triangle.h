01a7b00ae14b7cc11db3e50912f799b8eafa4ca4
Dan Nguyen <pedantic@gmail.com>
2019-06-23 19:02:36 -0500

    Add cpp/triangle

---
+++ cpp/triangle/triangle.h
@@ -0,0 +1,33 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+#include <algorithm>
+#include <array>
+#include <set>
+
+namespace triangle {
+enum flavor { equilateral, isosceles, scalene };
+
+template <class T> flavor kind(const T &a, const T &b, const T &c) {
+    std::array<T, 3> sides = {a, b, c};
+    std::sort(std::begin(sides), std::end(sides));
+
+    if (sides[0] + sides[1] <= sides[2] ||
+        std::any_of(std::begin(sides), std::end(sides),
+                    [](const T &i) { return i == T(); })) {
+        throw std::domain_error("Invalid triangle");
+    }
+
+    std::set<T> uniq_sides = {a, b, c};
+    if (uniq_sides.size() == 1) {
+        return flavor::equilateral;
+    } else if (uniq_sides.size() == 2) {
+        return flavor::isosceles;
+    } else {
+        return flavor::scalene;
+    }
+}
+
+} // namespace triangle
+
+#endif
