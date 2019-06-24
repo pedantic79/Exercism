6acdf2a8f9caa22b108f6d8b9717cba7d3a25757
Dan Nguyen <pedantic@gmail.com>
2019-06-23 22:15:33 -0500

    Add rust/accumulate

---
+++ rust/accumulate/src/lib.rs
@@ -0,0 +1,10 @@
+pub fn map<T, U, F>(input: Vec<T>, mut function: F) -> Vec<U> where
+    F: FnMut(T) -> U,
+{
+    let mut v = Vec::with_capacity(input.len());
+    for i in input {
+        v.push(function(i));
+    }
+
+    v
+}
