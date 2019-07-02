aeac4231409d83868e18859dc7855e3754f23fe1
Dan Nguyen <pedantic@gmail.com>
2019-07-02 13:17:33 -0500

    Update rust/luhn

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,46 +1,24 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    let sum = code
-                .chars()
-                .rev()
-                .fold(Some((0, 0)), |acc, letter| {
-                    if char::is_whitespace(letter) {
-                        return acc
-                    }
+    let sum =
+        code.chars()
+            .rev()
+            .filter(|c| !c.is_whitespace())
+            .try_fold((0, 0), |(s, idx), letter| {
+                let dgt = char::to_digit(letter, 10).map(|x| double_if_second(x, idx))?;
 
-                    if ! char::is_numeric(letter) {
-                        return None
-                    }
+                Some((s + dgt, idx + 1))
+            });
 
-                    acc.map(|(s, idx)| {
-                        let dgt = char::to_digit(letter, 10)
-                                        .map(|x| double_if_second(x, idx))
-                                        .unwrap();
-                        (s + dgt, idx + 1)
-                    })
-                });
-
-    match sum {
-        Some((s, l)) => {
-            if l < 2 {
-                false
-            } else {
-                s % 10 == 0
-            }
-        },
-        None => false,
-    }
+    sum.map_or(false, |(s, length)| length > 1 && s % 10 == 0)
 }
 
 fn double_if_second(num: u32, idx: u32) -> u32 {
-    if idx % 2 == 0 {
-        num
+    let n = if idx % 2 == 0 { num } else { num * 2 };
+
+    if n > 9 {
+        n - 9
     } else {
-        let doubled = num * 2;
-        if doubled > 9 {
-            doubled - 9
-        } else {
-            doubled
-        }
+        n
     }
 }
