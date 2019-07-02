8f8d1ad804ee454b4747ca32aa7bcc6df3ab9159
Dan Nguyen <pedantic@gmail.com>
2019-07-02 12:24:37 -0500

    Add graphmemes testvase to rust/reverse-string

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -3,3 +3,10 @@ use unicode_segmentation::UnicodeSegmentation;
 pub fn reverse(input: &str) -> String {
     input.graphemes(true).rev().collect()
 }
+
+mod tests {
+    #[test]
+    fn rosetta() {
+        assert_eq!(super::reverse("as⃝df̅"), "f̅ds⃝a");
+    }
+}
