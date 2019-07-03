5a8df0b3fa324aa519916fe32ebb8da4fc039466
Dan Nguyen <pedantic@gmail.com>
2019-07-03 13:52:11 -0500

    Update rust/gigasecond

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,8 +1,8 @@
-extern crate chrono;
-use chrono::{DateTime, Utc, Duration};
+use chrono::{DateTime, Duration, Utc};
+
+const GIGASECOND: i64 = 1_000_000_000;
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    let billion = Duration::seconds(1_000_000_000);
-    start.checked_add_signed(billion).unwrap()
+    start + Duration::seconds(GIGASECOND)
 }
