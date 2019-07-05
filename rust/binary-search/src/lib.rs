7e84df9106c8703960d360811e8ad4ef7089f993
Dan Nguyen <pedantic@gmail.com>
2019-07-04 21:37:47 -0500

    Add rust/binary-search

---
+++ rust/binary-search/src/lib.rs
@@ -0,0 +1,29 @@
+use std::cmp::Ordering;
+
+pub fn find<C, T>(container: C, key: T) -> Option<usize>
+where
+    C: AsRef<[T]>,
+    T: Ord,
+{
+    let mut container = container.as_ref();
+    let mut offset = 0;
+
+    loop {
+        let midpoint = container.len() / 2;
+        let (left, right) = container.split_at(midpoint);
+
+        if let Some(value) = right.first() {
+            match value.cmp(&key) {
+                Ordering::Less => {
+                    container = &right[1..];
+                    offset += left.len() + 1;
+                }
+                Ordering::Equal => return Some(midpoint + offset),
+                Ordering::Greater => container = left,
+            }
+        } else {
+            return None;
+        }
+    }
+
+}
