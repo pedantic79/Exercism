90570263554be844dbd92779777d26f2c73bc4c2
Dan Nguyen <pedantic@gmail.com>
2019-07-08 22:29:46 -0500

    Update rust/atbash-cipher

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -1,31 +1,35 @@
+use std::iter::once;
+
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    plain
-        .chars()
-        .filter(|x| x.is_ascii())
-        .filter(|x| x.is_alphanumeric())
-        .map(|x| x.to_ascii_lowercase())
-        .map(at_bash)
-        .collect::<Vec<char>>()
-        .chunks(5)
-        .map(|group| group.iter().collect::<String>())
-        .collect::<Vec<String>>()
-        .join(" ")
+    at_bash(plain)
+        .enumerate()
+        .flat_map(|(idx, c)| {
+            if idx % 5 == 0 && idx > 0 {
+                Some(' ')
+            } else {
+                None
+            }
+            .into_iter()
+            .chain(once(c))
+        })
+        .collect()
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    cipher
-        .chars()
-        .filter(|x| x.is_alphanumeric())
-        .map(at_bash)
-        .collect::<String>()
+    at_bash(cipher).collect()
 }
 
-pub fn at_bash(ch: char) -> char {
-    if ch.is_numeric() {
-        ch
-    } else {
-        (b'a' + b'z' - ch as u8) as char
-    }
+fn at_bash(s: &str) -> impl Iterator<Item = char> + '_ {
+    s.chars()
+        .filter(|c| c.is_ascii_alphanumeric())
+        .map(|c| c.to_ascii_lowercase())
+        .map(|c| {
+            if c.is_numeric() {
+                c
+            } else {
+                (b'a' + b'z' - c as u8) as char
+            }
+        })
 }
