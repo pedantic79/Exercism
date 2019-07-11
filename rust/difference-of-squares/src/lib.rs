cd4ff88b93e3d4ec50f00452cc34ef14da1273d4
Dan Nguyen <pedantic@gmail.com>
2019-07-11 16:49:47 -0500

    Update rust/difference-of-squares

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -1,9 +1,9 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    (n.pow(4) + 2 * n.pow(3) + n.pow(2)) / 4
+    (n * (n + 1) / 2).pow(2)
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    (n + 3 * n.pow(2) + 2 * n.pow(3)) / 6
+    n * (n + 1) * (2 * n + 1) / 6
 }
 
 pub fn difference(n: u32) -> u32 {
