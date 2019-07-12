f0bd87129295a2b7cde1354eaaceed0789cbf3cf
Dan Nguyen <pedantic@gmail.com>
2019-07-11 21:58:03 -0500

    Update rust/saddle-points

--- rust/saddle-points/src/lib.rs
+++ rust/saddle-points/src/lib.rs
@@ -3,14 +3,13 @@ pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
     let w = input[0].len();
 
     (0..h)
-        .flat_map(|x| (0..w).map(|y| (x, y)).collect::<Vec<(usize, usize)>>())
+        .flat_map(|x| (0..w).map(|y| (x, y)).collect::<Vec<_>>())
         .filter(|(r, c)| {
-            let cell = input[*r][*c];
-            let col: Vec<u64> = input.iter().map(|v| v[*c]).collect();
+            let cmin = input.iter().map(|v| v[*c]).min().unwrap();
+            let rmax = input[*r].iter().cloned().max().unwrap();
 
-            let rmax = input[*r].iter().max().unwrap();
-            let cmin = col.iter().min().unwrap();
-            cell >= *rmax && cell <= *cmin
+            let cell = input[*r][*c];
+            cell >= rmax && cell <= cmin
         })
-        .collect::<Vec<(usize, usize)>>()
+        .collect()
 }
