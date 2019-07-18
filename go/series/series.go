026fedc649d22949bab6dfb6e6d00df2c2287509
Dan Nguyen <pedantic@gmail.com>
2019-07-17 22:44:49 -0500

    Add go/series

---
+++ go/series/series.go
@@ -0,0 +1,22 @@
+package series
+
+func All(n int, s string) []string {
+	output := []string{}
+
+	for i := 0; i+n <= len(s); i++ {
+		output = append(output, s[i:i+n])
+	}
+	return output
+}
+
+func UnsafeFirst(n int, s string) string {
+	return s[:n]
+}
+
+func First(n int, s string) (first string, ok bool) {
+	if n > len(s) {
+		return "", false
+	}
+
+	return s[:n], true
+}
