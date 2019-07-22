43a27a83f33379b036ace30a7a5cc2f3e03ab396
Dan Nguyen <pedantic@gmail.com>
2019-07-22 16:00:49 -0500

    Add javascript/space-age

---
+++ javascript/space-age/space-age.js
@@ -0,0 +1,18 @@
+const EARTH_YEAR = 31557600;
+
+const orbitalPeriods = {
+  "earth": 1.00000000,
+  "mercury": 0.24084670,
+  "venus": 0.61519726,
+  "mars": 1.88081580,
+  "jupiter": 11.86261500,
+  "saturn": 29.44749800,
+  "uranus": 84.01684600,
+  "neptune": 164.79132000,
+};
+
+export const age = (planet, seconds) => {
+  return nearestHundredths(seconds / EARTH_YEAR / orbitalPeriods[planet]);
+};
+
+const nearestHundredths = (num) => Math.round(100 * num) / 100;
