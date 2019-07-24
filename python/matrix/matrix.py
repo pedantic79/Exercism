0fad555ad802302b27885f12d45134e49fe4c979
Dan Nguyen <pedantic@gmail.com>
2019-07-24 18:35:51 -0500

    Update python/matrix

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,7 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        self.matrix = []
-        for row in matrix_string.split('\n'):
-            r = [int(cell) for cell in row.split(' ')]
-            self.matrix.append(r)
+        self.matrix = [[int(cell) for cell in row.split()]
+                       for row in matrix_string.splitlines()]
 
     def row(self, index):
         return self.matrix[index - 1]
