301ea6a22be2bab4b38733057083ff92f4239350
Dan Nguyen <pedantic@gmail.com>
2019-08-01 10:40:40 -0500

    Update rust/forth

--- rust/forth/src/stack.rs
+++ rust/forth/src/stack.rs
@@ -31,34 +31,6 @@ impl Stack {
         }
     }
 
-    fn binary_op<F>(&mut self, f: F) -> ForthResult
-    where
-        F: (Fn(Value, Value) -> ValueResult),
-    {
-        let a = self.stack_pop()?;
-        let b = self.stack_pop()?;
-
-        let n = f(a, b)?;
-        self.stack.push(n);
-        Ok(())
-    }
-
-    pub fn op_add(&mut self) -> ForthResult {
-        self.binary_op(|a, b| Ok(a + b))
-    }
-
-    pub fn op_sub(&mut self) -> ForthResult {
-        self.binary_op(|a, b| Ok(b - a))
-    }
-
-    pub fn op_mul(&mut self) -> ForthResult {
-        self.binary_op(|a, b| Ok(b * a))
-    }
-
-    pub fn op_div(&mut self) -> ForthResult {
-        self.binary_op(|a, b| b.checked_div(a).ok_or(Error::DivisionByZero))
-    }
-
     pub fn op_dup(&mut self) -> ForthResult {
         let a = self.stack_peek(1)?;
         self.stack.push(a);
@@ -85,5 +57,33 @@ impl Stack {
 
         self.stack.push(a);
         Ok(())
+    }
+
+    fn binary_op<F>(&mut self, f: F) -> ForthResult
+    where
+        F: (Fn(Value, Value) -> ValueResult),
+    {
+        let a = self.stack_pop()?;
+        let b = self.stack_pop()?;
+
+        let n = f(a, b)?;
+        self.stack.push(n);
+        Ok(())
+    }
+
+    pub fn op_add(&mut self) -> ForthResult {
+        self.binary_op(|a, b| Ok(a + b))
+    }
+
+    pub fn op_sub(&mut self) -> ForthResult {
+        self.binary_op(|a, b| Ok(b - a))
+    }
+
+    pub fn op_mul(&mut self) -> ForthResult {
+        self.binary_op(|a, b| Ok(b * a))
+    }
+
+    pub fn op_div(&mut self) -> ForthResult {
+        self.binary_op(|a, b| b.checked_div(a).ok_or(Error::DivisionByZero))
     }
 }
