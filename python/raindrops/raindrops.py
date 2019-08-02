3dabd88e0dc73f1194cc0228d9601a108c57b7c9
Dan Nguyen <pedantic@gmail.com>
2019-08-02 18:56:02 -0500

    Update python/raindrops

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -2,7 +2,4 @@ def convert(number):
     s = ''.join(sound for (denom, sound) in
                 [(3, "Pling"), (5, "Plang"), (7, "Plong")] if number % denom == 0)
 
-    if not s:
-        return str(number)
-    else:
-        return s
+    return s if s else str(number)
