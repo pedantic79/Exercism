a4215f6f725e8dc9894c1cc4f5af1f302add9eb7
Dan Nguyen <pedantic@gmail.com>
2019-08-01 23:19:10 -0500

    Update rust/pythagorean-triplet to latest

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -1,11 +1,14 @@
-pub fn find() -> Option<u32> {
-    for a in 1..500 {
-        for b in a..500 {
-            let c = 1000 - a - b;
-            if a * a + b * b == c * c {
-                return Some(a * b * c);
-            }
-        }
-    }
-    None
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use std::collections::HashSet;
+
+pub fn find(sum: u32) -> HashSet<[u32; 3]> {
+    (1..(sum / 3))
+        .into_par_iter()
+        .map(|a| {
+            let b_c = sum - a;
+            let b = (b_c * b_c - a * a) / (2 * b_c);
+            [a, b, b_c - b]
+        })
+        .filter(|[a, b, c]| a < b && a * a + b * b == c * c)
+        .collect()
 }
