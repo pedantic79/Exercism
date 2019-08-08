cf076d41db6cccc9dcaed8ad951f962414c6c028
Dan Nguyen <pedantic@gmail.com>
2019-08-07 23:08:40 -0500

    Update rust/luhn

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,20 +1,18 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    let sum =
-        code.chars()
-            .rev()
-            .filter(|c| !c.is_whitespace())
-            .try_fold((0, 0), |(s, idx), letter| {
-                let dgt = letter.to_digit(10).map(|x| double_if_second(x, idx))?;
+    code.chars()
+        .rev()
+        .filter(|c| !c.is_whitespace())
+        .try_fold((0, 0), |(s, idx), letter| {
+            let dgt = letter.to_digit(10).map(|x| double_if_second(x, idx))?;
 
-                Some((s + dgt, idx + 1))
-            });
-
-    sum.map_or(false, |(s, length)| length > 1 && s % 10 == 0)
+            Some((s + dgt, idx + 1))
+        })
+        .map_or(false, |(s, length)| length > 1 && s % 10 == 0)
 }
 
 fn double_if_second(num: u32, idx: u32) -> u32 {
-    let n = if idx % 2 == 0 { num } else { num * 2 };
+    let n = num * (idx % 2 + 1);
 
     if n > 9 {
         n - 9
