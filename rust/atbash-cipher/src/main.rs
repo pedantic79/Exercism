74fbe52a7e1a28b0732861fd08b3f08fde3fbc0e
Dan Nguyen <pedantic@gmail.com>
2019-08-17 12:55:21 -0500

    Update rust/atbash-cipher

--- rust/atbash-cipher/src/main.rs
+++ rust/atbash-cipher/src/main.rs
@@ -2,11 +2,11 @@ use atbash_cipher::*;
 use easybench::bench;
 
 macro_rules! benchmark {
-    ($name:literal, $len:expr, $ben:ident, $input:ident) => {
+    ($name:literal, $ben:ident, $input:ident) => {
         println!(
-            "{:>8}/{:<4} {}",
+            "{:>9}/{:<4}{}",
             $name,
-            $len,
+            $input.len(),
             bench(|| $ben($input.chars()))
         );
     };
@@ -16,13 +16,14 @@ fn main() {
     for len in &[12, 48, 252, 1024] {
         let input = String::from_utf8(vec![b'X'; *len as usize]).unwrap();
 
-        benchmark!("vec", len, split_vec, input);
-        benchmark!("iterator", len, split_flat_map, input);
-        benchmark!("fold_str", len, split_str_push, input);
-        benchmark!("from_fn", len, split_from_fn, input);
+        benchmark!("vec", split_vec, input);
+        benchmark!("iterator", split_flat_map, input);
+        benchmark!("fold_str", split_str_push, input);
+        benchmark!("from_fn", split_from_fn, input);
     }
 }
 
+#[allow(dead_code)]
 fn split_vec(iter: impl Iterator<Item = char>) -> String {
     let v = iter.collect::<Vec<char>>();
     v.chunks(5)
@@ -31,6 +32,7 @@ fn split_vec(iter: impl Iterator<Item = char>) -> String {
         .join(" ")
 }
 
+#[allow(dead_code)]
 fn split_str_push(iter: impl Iterator<Item = char>) -> String {
     iter.enumerate().fold(String::new(), |mut s, (p, c)| {
         if p % 5 == 0 && p > 0 {
