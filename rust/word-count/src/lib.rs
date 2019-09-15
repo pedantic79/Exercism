cca985897813c407c03b3e398c7e5740ad615a2d
Dan Nguyen <pedantic@gmail.com>
2019-09-15 12:47:33 -0500

    Update rust/word-count

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -1,12 +1,14 @@
 use std::collections::HashMap;
 
+const QUOTE: char = '\'';
+
 /// Count occurrences of words.
 pub fn word_count(words: &str) -> HashMap<String, u32> {
     words
-        .split(|c: char| !c.is_alphanumeric() && c != '\'')
+        .split(|c: char| !c.is_alphanumeric() && c != QUOTE)
         .filter(|s| !s.is_empty())
         .fold(HashMap::new(), |mut hm, word| {
-            let word = word.trim_matches('\'').to_lowercase();
+            let word = word.trim_matches(QUOTE).to_lowercase();
             *hm.entry(word).or_insert(0) += 1;
             hm
         })
