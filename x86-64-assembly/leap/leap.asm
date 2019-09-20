64fa8dab41db285e1dbb57056c5d0764ccad81ce
Dan Nguyen <pedantic@gmail.com>
2019-09-20 00:40:30 -0500

    Add x86-64-assembly/leap

---
+++ x86-64-assembly/leap/leap.asm
@@ -0,0 +1,29 @@
+section .text
+global leap_year
+leap_year:
+    xor eax, eax        ; set default return to 0 (FALSE)
+    test dil, 3         ; Check bottom 2 bits to see if they are set
+    jne .end            ; ZF=1 if any bit set, jump to end if divisible by 4
+    mov ebx, edi        ; move input to EBX
+
+    ; Used https://www.hackersdelight.org/magic.htm to determine magic number
+    ; and shift amount to divide by 100 using multiplication and shifting
+    imul rcx, rbx, 0x51EB851F
+    shr rcx, 37
+
+    ; To calculate the modulo, we'll multiply the quotient by the divisor
+    ; If it's the same as edi, that means the modulo is 0
+    imul ecx, ecx, 100
+    mov al, 1           ; Set the return to TRUE just in-case
+    cmp edi, ecx        ; compare if they are equal
+    jne .end            ; if they're note equal return TRUE
+
+    ; Same as above, but shift by 39 to do /400
+    imul rcx, rbx, 0x51EB851F
+    shr rcx, 39
+
+    imul ecx, ecx, 400  ; multiply by 400 to do the same muliplier
+    cmp edi, ecx
+    sete al             ; sets the A register to 0 if divisible by 400, 1 if not
+.end:
+    ret
