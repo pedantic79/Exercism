2f0fcc34b7b31dd4651b56be9117cad1777b3cd3
Dan Nguyen <pedantic@gmail.com>
2019-09-21 10:55:22 -0500

    Update javascript/robot-name

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -1,6 +1,7 @@
 const ALPHA = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ']
 const ROBOTS = []
 
+// Generate the array of all robot names
 ALPHA.forEach((a) => {
     ALPHA.forEach((b) => {
         for (let i = 0; i < 1000; i++) {
@@ -9,6 +10,7 @@ ALPHA.forEach((a) => {
     })
 })
 
+// This keeps track of what robot names have been given out
 let position = ROBOTS.length
 
 export class Robot {
@@ -21,6 +23,11 @@ Robot.releaseNames = () => {
     position = ROBOTS.length
 };
 
+// This randomly selects a name from the list of available names
+// (between 0 and position). The name is then swapped with the unused name at
+// the end and position is decremented. This is basically a Fisher-Yates
+// shuffle. Rather than precomputing the shuffle, we are randomly selecting
+// the name as they are asked for.
 const generateName = () => {
     if (position > 0) {
         const r = Math.floor(Math.random() * position)
