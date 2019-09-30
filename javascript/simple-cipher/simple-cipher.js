aece70837a65389e48bfc29525e119d031e0cd06
Dan Nguyen <pedantic@gmail.com>
2019-09-30 10:34:39 -0500

    Update javascript/simple-cipher

--- javascript/simple-cipher/simple-cipher.js
+++ javascript/simple-cipher/simple-cipher.js
@@ -1,3 +1,5 @@
+const SHIFT_RIGHT_PROC = (letter, shift) => letter + shift
+const SHIFT_LEFT_PROC = (letter, shift) => letter - shift
 
 export class Cipher {
   constructor(key) {
@@ -5,11 +7,11 @@ export class Cipher {
   }
 
   encode(msg) {
-    return encode_decode(msg, this.keyArray, (a, b) => a + b)
+    return encode_decode(msg, this.keyArray, SHIFT_RIGHT_PROC)
   }
 
   decode(msg) {
-    return encode_decode(msg, this.keyArray, (a, b) => a - b)
+    return encode_decode(msg, this.keyArray, SHIFT_LEFT_PROC)
   }
 
   get key() {
@@ -17,18 +19,24 @@ export class Cipher {
   }
 }
 
+const A = 'a'.charCodeAt()
+const char2val = (character) => character.charCodeAt() - A
+const val2char = (number) => String.fromCharCode(number + A)
+const eculidian_remainder = (dividend, divisor) => (dividend + divisor) % divisor
+
 const RANDOM_KEY_LENGTH = 100;
 const ALPHABET_LENGTH = 26;
-const A = 'a'.charCodeAt()
-const char2val = (c) => c.charCodeAt() - A
-const val2char = (i) => String.fromCharCode(i + A)
 
-
-const encode_decode = (msg, key, op) => {
-  return msg.split('').map((c, idx) =>
-    val2char((ALPHABET_LENGTH + op(char2val(c), key[idx % key.length])) % ALPHABET_LENGTH)).join('')
+const encode_decode = (msg, key, letter_shifter) => {
+  return msg.split('').map(
+    (character, idx) => {
+      const shifted_letter = letter_shifter(char2val(character), key[idx % key.length])
+      const within_range = eculidian_remainder(shifted_letter, ALPHABET_LENGTH)
+      return val2char(within_range)
+    }
+  ).join('')
 }
 
 const randomKey = () => {
-  return Array.from(new Array(RANDOM_KEY_LENGTH), () => Math.floor(Math.random() * ALPHABET_LENGTH))
+  return Array.from({ length: RANDOM_KEY_LENGTH }, () => Math.floor(Math.random() * ALPHABET_LENGTH))
 }
