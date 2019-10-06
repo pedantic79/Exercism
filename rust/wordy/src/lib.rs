adb8a0a5203e57eb60996b6b1884d99b60ccd5f5
Dan Nguyen <pedantic@gmail.com>
2019-10-05 21:27:36 -0500

    Update rust/wordy

--- rust/wordy/src/lib.rs
+++ rust/wordy/src/lib.rs
@@ -23,15 +23,7 @@ impl<'a> Tokens<'a> {
     }
 
     fn parse_ordinal(&mut self) -> Option<u32> {
-        let s = self.0.next()?;
-        let (left, right) = s.split_at(s.len() - 2);
-
-        let num = left.parse().ok()?;
-        match (num % 10, right) {
-            (1, "st") | (2, "nd") | (3, "rd") => Some(num),
-            (_, "th") => Some(num),
-            _ => None,
-        }
+        parse_ordinal(self.0.next()?)
     }
 
     fn empty(&mut self) -> bool {
@@ -83,4 +75,50 @@ pub fn answer(command: &str) -> Option<i32> {
     }
 
     Some(answer)
+}
+
+fn parse_ordinal(s: &str) -> Option<u32> {
+    if s.len() < 3 {
+        None
+    } else {
+        let (left, right) = s.split_at(s.len() - 2);
+
+        let num = left.parse().ok()?;
+        let tens = (num % 100) / 10;
+
+        match (num % 10, right) {
+            (0, "th") | (1, "st") | (2, "nd") | (3, "rd") if tens != 1 => Some(num),
+            (n, "th") if n > 3 && tens != 1 => Some(num),
+            (_, "th") if tens == 1 => Some(num),
+            _ => None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::parse_ordinal;
+    #[test]
+    fn test_valid_ordinals() {
+        let ordinals = [
+            "1st", "2nd", "3rd", "4th", "9th", "10th", "11th", "12th", "13th", "20th", "21st",
+            "22nd", "23rd", "24th", "30th", "31st", "101st", "111th", "121st",
+        ];
+
+        for o in ordinals.iter() {
+            assert!(parse_ordinal(o).is_some())
+        }
+    }
+
+    #[test]
+    fn test_invalid_ordinals() {
+        let ordinals = [
+            "1th", "2th", "3th", "4st", "9nd", "10rd", "11st", "12nd", "13rd", "20st", "21th",
+            "22th", "23th", "24rd", "30nd", "31th", "101th", "111st", "121th", "20fj", "1jkl",
+        ];
+
+        for o in ordinals.iter() {
+            assert!(parse_ordinal(o).is_none())
+        }
+    }
 }
