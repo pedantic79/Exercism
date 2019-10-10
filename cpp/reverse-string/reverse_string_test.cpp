91718b26f11beaeded92f8e65cbbaff6b08a5a21
Dan Nguyen <pedantic@gmail.com>
2019-10-09 23:57:42 -0500

    Update cpp/reverse-string

--- cpp/reverse-string/reverse_string_test.cpp
+++ cpp/reverse-string/reverse_string_test.cpp
@@ -1,24 +1,29 @@
 #include "reverse_string.h"
 #include "test/catch.hpp"
 
-TEST_CASE("an_empty_string") {
+TEST_CASE("an_empty_string")
+{
     REQUIRE("" == reverse_string::reverse_string(""));
 }
 
-#if defined(EXERCISM_RUN_ALL_TESTS)
-TEST_CASE("a_word") {
+//#if defined(EXERCISM_RUN_ALL_TESTS)
+TEST_CASE("a_word")
+{
     REQUIRE("tobor" == reverse_string::reverse_string("robot"));
 }
 
-TEST_CASE("a_capitalized_word") {
+TEST_CASE("a_capitalized_word")
+{
     REQUIRE("nemaR" == reverse_string::reverse_string("Ramen"));
 }
 
-TEST_CASE("a_sentence_with_punctuation") {
+TEST_CASE("a_sentence_with_punctuation")
+{
     REQUIRE("!yrgnuh m'I" == reverse_string::reverse_string("I'm hungry!"));
 }
 
-TEST_CASE("a_palindrome") {
-    REQUIRE("racecar" == reverse_string::reverse_string("racecar"));
+TEST_CASE("a_palindrome")
+{
+   REQUIRE("racecar" == reverse_string::reverse_string("racecar"));
 }
-#endif
+//#endif
