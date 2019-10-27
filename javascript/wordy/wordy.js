4d36d584c809605773cc6322235816d9c6bcabad
Dan Nguyen <pedantic@gmail.com>
2019-10-26 19:01:22 -0500

    Update javascript/wordy

--- javascript/wordy/wordy.js
+++ javascript/wordy/wordy.js
@@ -1,66 +1,67 @@
 export class ArgumentError {
-    constructor(message) {
-        this.name = 'ArgumentError'
-        this.message = message
-    }
+  constructor(message) {
+    this.name = "ArgumentError";
+    this.message = message;
+  }
 }
-
 export class WordProblem {
-    constructor(input) {
-        this.input = input
+  constructor(input) {
+    this.input = input;
+  }
+
+  answer() {
+    if (!this.input.endsWith("?")) {
+      throw new ArgumentError(`parse error: ${this.input} does not end with ?`);
     }
 
-    answer() {
-        if (!this.input.endsWith('?')) {
-            throw new ArgumentError(`parse error: ${this.input} does not end with ?`)
-        }
+    const tokens = this.input.replace(/\?$/, "").split(/ +/);
 
-        const tokens = this.input.replace(/\?$/, '').split(/ +/)
+    check_token("What", tokens.shift());
+    check_token("is", tokens.shift());
+    let total = parse_num(tokens.shift());
 
-        check_token('What', tokens.shift())
-        check_token('is', tokens.shift())
-        let total = parse_num(tokens.shift())
-
-        while (tokens.length > 0) {
-            const operation = parse_op(tokens)
-            const number = parse_num(tokens.shift())
-            total = operation(total, number)
-        }
-        return total
+    while (tokens.length > 0) {
+      const operation = parse_op(tokens);
+      const number = parse_num(tokens.shift());
+      total = operation(total, number);
     }
+    return total;
+  }
 }
 
 const check_token = (expected, actual) => {
-    if (expected !== actual) {
-        throw new ArgumentError(`parse error: '${expected}' is expected, found ${actual}`)
-    }
-}
+  if (expected !== actual) {
+    throw new ArgumentError(
+      `parse error: '${expected}' is expected, found ${actual}`
+    );
+  }
+};
 
-const parse_num = (token) => {
-    const number = token.match(/^(-?\d+)$/)
-    if (number === undefined) {
-        throw new ArgumentError(`parse error: invalid number ${token}`)
-    }
-    return Number(number[1])
-}
+const parse_num = token => {
+  const number = token.match(/^(-?\d+)$/);
+  if (number === undefined) {
+    throw new ArgumentError(`parse error: invalid number ${token}`);
+  }
+  return Number(number[1]);
+};
 
 const OPERATIONS = {
-    'plus': (total, value) => total + value,
-    'minus': (total, value) => total - value,
-    'multiplied': (total, value) => total * value,
-    'divided': (total, value) => total / value
-}
+  plus: (total, value) => total + value,
+  minus: (total, value) => total - value,
+  multiplied: (total, value) => total * value,
+  divided: (total, value) => total / value
+};
 
-const parse_op = (tokens) => {
-    const token = tokens.shift()
-    if (token === 'multiplied' || token === 'divided') {
-        check_token('by', tokens.shift())
-    }
+const parse_op = tokens => {
+  const token = tokens.shift();
+  if (token === "multiplied" || token === "divided") {
+    check_token("by", tokens.shift());
+  }
 
-    const operation = OPERATIONS[token]
-    if (operation === undefined) {
-        throw new ArgumentError(`parse error: unknown operation ${token}`)
-    }
+  const operation = OPERATIONS[token];
+  if (operation === undefined) {
+    throw new ArgumentError(`parse error: unknown operation ${token}`);
+  }
 
-    return operation
-}
+  return operation;
+};
