b35dd621b4e75ea916efd3759e60eb34ec2a071d
Dan Nguyen <pedantic@gmail.com>
2019-10-28 20:02:30 -0500

    Update rust/reverse-string

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,7 +1,11 @@
-use unicode_segmentation::UnicodeSegmentation;
+// unicode-segmentation does not properly handle the most modern emoji
+// specifically the ZWJ. So I'm using unic_segment which does.
+// use unicode_segmentation::UnicodeSegmentation;
+use unic_segment::Graphemes;
 
 pub fn reverse(input: &str) -> String {
-    input.graphemes(true).rev().collect()
+    // input.graphemes(true).rev().collect()
+    Graphemes::new(input).rev().collect()
 }
 
 #[cfg(test)]
@@ -9,5 +13,17 @@ mod tests {
     #[test]
     fn rosetta() {
         assert_eq!(super::reverse("as⃝df̅"), "f̅ds⃝a");
+    }
+
+    #[test]
+    fn emoji() {
+        assert_eq!(super::reverse("🤦🏼👪"), "👪🤦🏼");
+    }
+
+    #[test]
+    fn emoji12() {
+        // This emoji is one grapheme, so the reverse should be the same
+        // https://emojipedia.org/factory-worker-medium-skin-tone/
+        assert_eq!(super::reverse("🧑🏽‍🏭"), "🧑🏽‍🏭")
     }
 }
