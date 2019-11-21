c8beea7c02d51130f5b2a2d251b355cee2235ad2
Dan Nguyen <pedantic@gmail.com>
2019-11-20 20:37:12 -0600

    Update rust/clock

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,37 +1,33 @@
 use std::fmt;
 
 #[derive(Debug, Eq, PartialEq)]
-pub struct Clock {
-    minutes: i32,
-}
+pub struct Clock(i32);
 
 const MINUTES_PER_DAY: i32 = 60 * 24;
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let hrs = self.minutes / 60;
-        let mnts = self.minutes % 60;
+        let (hrs, mnts) = Self::m_to_hm(self.0);
         write!(f, "{:02}:{:02}", hrs, mnts)
     }
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let minutes = Self::adjust(hours * 60 + minutes);
-        Clock { minutes }
+        Clock(Self::hm_to_m(hours, minutes))
     }
 
     pub fn add_minutes(self, minutes: i32) -> Self {
-        Clock::new(0, self.minutes + minutes)
+        Self::new(0, self.0 + minutes)
     }
 
-    fn adjust(minutes: i32) -> i32 {
-        if minutes < 0 {
-            // Determine how many times we need to add 1440 to make `minutes` positive
-            let magnitute = minutes.abs() / MINUTES_PER_DAY + 1;
-            minutes + MINUTES_PER_DAY * magnitute
-        } else {
-            minutes % MINUTES_PER_DAY
-        }
+    // As of Rust 1.38, there is now a eucliedan division and remainder
+    fn hm_to_m(hours: i32, minutes: i32) -> i32 {
+        let minutes = hours * 60 + minutes;
+        minutes.rem_euclid(MINUTES_PER_DAY)
+    }
+
+    fn m_to_hm(minutes: i32) -> (i32, i32) {
+        (minutes / 60, minutes % 60)
     }
 }
