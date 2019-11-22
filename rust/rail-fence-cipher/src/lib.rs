c95d036018370d5d4593ba07097e20b12f721d8c
Dan Nguyen <pedantic@gmail.com>
2019-11-22 15:17:36 -0600

    Update rust/rail-fence-cipher

--- rust/rail-fence-cipher/src/lib.rs
+++ rust/rail-fence-cipher/src/lib.rs
@@ -12,30 +12,31 @@ impl RailFence {
     pub fn encode(&self, text: &str) -> String {
         let mut fence = self.mk_fence();
 
+        // Place each character on the proper RailFence rail
         for (idx, c) in self.indices().zip(text.chars()) {
             fence[idx].push(c);
         }
 
+        // Combine into a String
         fence.into_iter().collect()
     }
 
     pub fn decode(&self, cipher: &str) -> String {
         let len = cipher.len();
-        let indices_freq: Vec<usize> =
-            self.indices()
-                .take(len)
-                .fold(vec![0; self.0], |mut freq, idx| {
-                    freq[idx] += 1;
-                    freq
-                });
 
         let mut fence = self.mk_fence();
         let mut pos = 0;
-        for (rail, frequency) in fence.iter_mut().zip(indices_freq) {
+
+        // Copies slices of the string into the proper RailFence rail
+        // We're copying in reverse order, because we're going to pop everything
+        // off to build the output string
+        for (rail, frequency) in fence.iter_mut().zip(self.indices_count(len)) {
             rail.extend(cipher[pos..pos + frequency].chars().rev());
             pos += frequency;
         }
 
+        // Use the RailFence to rebuild the String by popping the characters off
+        // in reverse order
         self.indices()
             .take(len)
             .fold(String::with_capacity(len), |mut s, idx| {
@@ -44,38 +45,20 @@ impl RailFence {
             })
     }
 
+    // Iterator that returns which rail to use, going from 0 to self.0
+    // and then back to 0.
     fn indices(&self) -> impl Iterator<Item = usize> {
         let l = self.0.saturating_sub(1);
         (0..=l).chain((1..l).rev()).cycle()
     }
 
-    pub fn indices2(&self) -> impl Iterator<Item = usize> {
-        let l = self.0;
-        let mut n = 1;
-        let mut direction = Direction::Down;
-
-        from_fn(move || {
-            n = match direction {
-                Direction::Up if n + 1 < l => n + 1,
-                Direction::Up => {
-                    direction = Direction::Down;
-                    n - 1
-                }
-                Direction::Down if n > 0 => n - 1,
-                Direction::Down => {
-                    direction = Direction::Up;
-                    n + 1
-                }
-            };
-
-            Some(n)
-        })
+    // Builds a Vec [M; self.0] in size that includes the length of each rail
+    fn indices_count(&self, size: usize) -> Vec<usize> {
+        self.indices()
+            .take(size)
+            .fold(vec![0; self.0], |mut freq, idx| {
+                freq[idx] += 1;
+                freq
+            })
     }
-}
-
-use std::iter::from_fn;
-
-enum Direction {
-    Up,
-    Down,
 }
