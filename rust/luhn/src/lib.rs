2b66691165cf28022990866d5cac7d6f1a07e60a
Dan Nguyen <pedantic@gmail.com>
2019-12-08 12:53:20 -0600

    Update rust/luhn

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -4,19 +4,17 @@ pub fn is_valid(code: &str) -> bool {
         .rev()
         .filter(|c| !c.is_whitespace())
         .try_fold((0, 0), |(s, idx), letter| {
-            let dgt = letter.to_digit(10).map(|x| double_if_second(x, idx))?;
-
-            Some((s + dgt, idx + 1))
+            letter
+                .to_digit(10)
+                .map(|x| (s + double_if_second(x, idx), idx + 1))
         })
         .map_or(false, |(s, length)| length > 1 && s % 10 == 0)
 }
 
 fn double_if_second(num: u32, idx: u32) -> u32 {
-    let n = num * (idx % 2 + 1);
-
-    if n > 9 {
-        n - 9
+    if idx % 2 == 1 {
+        num * 2 - if num > 4 { 9 } else { 0 }
     } else {
-        n
+        num
     }
 }
