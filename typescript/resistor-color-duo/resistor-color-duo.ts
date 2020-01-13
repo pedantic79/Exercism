9e80f0f66a4e82083b790cedad459ac0541710df
Dan Nguyen <pedantic@gmail.com>
2020-01-13 10:32:47 -0600

    Update typescript/resistor-color-duo based on mentor feedback

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,29 +1,48 @@
-const COLORS = [
-  "black",
-  "brown",
-  "red",
-  "orange",
-  "yellow",
-  "green",
-  "blue",
-  "violet",
-  "grey",
-  "white"
-];
-
 export class ResistorColor {
+  private static readonly COLORS = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white"
+  ];
+
+  private static readonly MAX_RESISTOR_COUNT: number = 2;
+
   private colors: string[];
 
   constructor(colors: string[]) {
-    if (colors.length < 2) {
+    if (colors.length < ResistorColor.MAX_RESISTOR_COUNT) {
       throw new Error("At least two colors need to be present");
     }
-    this.colors = colors;
+    this.colors = colors.slice(0, ResistorColor.MAX_RESISTOR_COUNT);
+  }
+
+  private getResistorValue(resistorColor: string): number {
+    return ResistorColor.COLORS.findIndex(
+      currentColor => currentColor === resistorColor
+    );
+  }
+
+  private buildTotalResitorValue(
+    totalRestistorValue: number,
+    currentResistorValue: number
+  ): number {
+    return totalRestistorValue * 10 + currentResistorValue;
   }
 
   value = (): number =>
-    this.colors
-      .slice(0, 2)
-      .map(color => COLORS.findIndex(c => c === color))
-      .reduce((currentNumber, newNumber) => currentNumber * 10 + newNumber);
+    this.colors.reduce((totalRestistorValue, currentResistorColor) => {
+      const currentResistorValue = this.getResistorValue(currentResistorColor);
+
+      return this.buildTotalResitorValue(
+        totalRestistorValue,
+        currentResistorValue
+      );
+    }, 0);
 }
