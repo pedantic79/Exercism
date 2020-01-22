4ca9de7b2af3c2659f4fa58d21371f5760896991
Dan Nguyen <pedantic@gmail.com>
2020-01-21 21:27:22 -0600

    Update rust/protein-translation

--- rust/protein-translation/src/lib.rs
+++ rust/protein-translation/src/lib.rs
@@ -10,30 +10,34 @@ impl<'a> CodonsInfo<'a> {
     }
 
     pub fn of_rna(&self, rna: &str) -> Option<Vec<&'a str>> {
-        rna.chars()
-            .collect::<Vec<_>>()
-            .chunks(3)
-            .map(|codon| {
-                let c = codon.iter().collect::<String>();
-                self.name_for(c.as_str())
-            })
-            .take_while(|x| {
-                if let Some(s) = x {
-                    *s != "stop codon"
-                } else {
-                    true
-                }
-            })
+        splitn(rna, 3)
+            .map(|codon| self.name_for(codon))
+            .take_while(|&x| Some("stop codon") != x)
             .collect()
     }
 }
 
 pub fn parse<'a>(pairs: Vec<(&'a str, &'a str)>) -> CodonsInfo<'a> {
-    let mut codons = HashMap::new();
-
-    for (codon, name) in pairs.into_iter() {
-        codons.insert(codon, name);
-    }
+    let codons = pairs.into_iter().collect();
 
     CodonsInfo { codons }
+}
+
+// Split Iterator, providing substrings of length `n`
+fn splitn(s: &str, n: usize) -> impl Iterator<Item = &str> + '_ {
+    let mut pos = 0;
+    let size = s.len();
+
+    std::iter::from_fn(move || {
+        let start = pos;
+        pos += n;
+
+        if pos < size {
+            Some(&s[start..pos])
+        } else if start < size {
+            Some(&s[start..])
+        } else {
+            None
+        }
+    })
 }
