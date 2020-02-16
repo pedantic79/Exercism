bd497a9fc49818bd7e51c72f76f3a6eb6270a99a
Dan Nguyen <pedantic@gmail.com>
2020-02-16 16:18:20 -0600

    Update cpp/*

--- cpp/nucleotide-count/nucleotide_count.h
+++ cpp/nucleotide-count/nucleotide_count.h
@@ -3,44 +3,44 @@
 #include <map>
 #include <string>
 
-namespace dna {
-    class counter {
-        std::string s;
+namespace nucleotide_count {
+class counter {
+    std::string s;
 
-        static void is_valid(char c) {
-            switch (c) {
-            case 'A':
-            case 'T':
-            case 'C':
-            case 'G':
-                return;
-            }
-
-            throw std::invalid_argument{"invalid nucleotide"};
+    static void is_valid(char c) {
+        switch (c) {
+        case 'A':
+        case 'T':
+        case 'C':
+        case 'G':
+            return;
         }
 
-      public:
-        counter(const std::string &dna_sequence) : s{dna_sequence} {
-            std::for_each(std::begin(s), std::end(s), is_valid);
+        throw std::invalid_argument{"invalid nucleotide"};
+    }
+
+  public:
+    counter(const std::string &dna_sequence) : s{dna_sequence} {
+        std::for_each(std::begin(s), std::end(s), is_valid);
+    }
+
+    int count(char nucleotide) const {
+        is_valid(nucleotide);
+
+        return std::count_if(
+            std::begin(s), std::end(s),
+            [nucleotide](const char c) { return c == nucleotide; });
+    }
+
+    std::map<char, int> nucleotide_counts() const {
+        std::map<char, int> ret{{'A', 0}, {'T', 0}, {'C', 0}, {'G', 0}};
+
+        for (char c : s) {
+            ret[c]++;
         }
+        return ret;
+    }
+};
 
-        int count(char nucleotide) const {
-            is_valid(nucleotide);
-
-            return std::count_if(
-                std::begin(s), std::end(s),
-                [nucleotide](const char c) { return c == nucleotide; });
-        }
-
-        std::map<char, int> nucleotide_counts() const {
-            std::map<char, int> ret{{'A', 0}, {'T', 0}, {'C', 0}, {'G', 0}};
-
-            for (char c : s) {
-                ret[c]++;
-            }
-            return ret;
-        }
-    };
-
-} // namespace dna
+} // namespace nucleotide_count
 #endif
