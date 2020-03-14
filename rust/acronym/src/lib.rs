9dc19d01239685535c35fbd428c755994bda684a
Dan Nguyen <pedantic@gmail.com>
2020-03-14 12:26:48 -0500

    Update rust/acronym to latest version and fix

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -1,18 +1,17 @@
 pub fn abbreviate(phrase: &str) -> String {
     phrase
-        .split(|c: char| !c.is_ascii_alphabetic())
+        .split(|c: char| !c.is_ascii_alphabetic() && c != '\'')
         .filter_map(|word| {
             let uppercase: String = word.chars().filter(|c| c.is_ascii_uppercase()).collect();
 
-            match uppercase.len() {
-                _ if uppercase.len() == word.len() => {
-                    uppercase.chars().nth(0).map(|c| c.to_string())
-                }
-                0 => word
-                    .chars()
-                    .nth(0)
-                    .map(|c| c.to_ascii_uppercase().to_string()),
-                _ => Some(uppercase),
+            if uppercase.is_empty() {
+                word.chars()
+                    .next()
+                    .map(|c| c.to_ascii_uppercase().to_string())
+            } else if uppercase.len() == word.len() {
+                uppercase.chars().next().map(|c| c.to_string())
+            } else {
+                Some(uppercase)
             }
         })
         .collect()
