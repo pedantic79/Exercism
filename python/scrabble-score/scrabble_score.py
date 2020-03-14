f53d73d49c5146cd32ab54a0f32fe664af13e755
Dan Nguyen <pedantic@gmail.com>
2020-03-14 12:10:06 -0500

    Add python/scrabble-score

---
+++ python/scrabble-score/scrabble_score.py
@@ -0,0 +1,17 @@
+STRING_TO_SCORES = {
+    "AEIOULNRST": 1,
+    "DG": 2,
+    "BCMP": 3,
+    "FHVWY": 4,
+    "K": 5,
+    "JX": 8,
+    "QZ": 10,
+}
+
+LETTER_VALUES = {
+    letter: score for (string, score) in STRING_TO_SCORES.items() for letter in string
+}
+
+
+def score(word):
+    return sum(LETTER_VALUES[character] for character in word.upper())
