a5ff5290f893fb6160e712039c2638bb098d89b7
Dan Nguyen <pedantic@gmail.com>
2020-03-24 08:12:54 -0500

    Add haskell/word-count

---
+++ haskell/word-count/src/WordCount.hs
@@ -0,0 +1,14 @@
+module WordCount (wordCount) where
+import Data.Bool (bool)
+import Data.Char (isAlphaNum, toLower)
+import Data.List (group, init, last, sort)
+import Data.Monoid (All(..), Any(..), getAll, getAny)
+
+wordCount :: String -> [(String, Int)]
+wordCount = map ((,) <$> head <*> length) . group . sort . fmap removeQuote . words . fmap normalize
+
+normalize :: Char -> Char
+normalize = bool <$> const ' ' <*> toLower <*> getAny . foldMap (Any .) [('\'' ==), isAlphaNum]
+
+removeQuote :: String -> String
+removeQuote = bool <$> id <*> init . tail <*> getAll . foldMap (All .) [('\'' ==) . head, ('\'' ==) . last]
