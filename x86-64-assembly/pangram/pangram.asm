b625837017038bd7420c516ab177b25f5762f2a7
Dan Nguyen <pedantic@gmail.com>
2020-03-30 00:23:44 -0500

    Add x86-64-assembly/pangram

---
+++ x86-64-assembly/pangram/pangram.asm
@@ -0,0 +1,39 @@
+section .text
+global is_pangram
+is_pangram:
+    xor r8d, r8d                ; answer
+    mov r9, -1                  ; pos
+    mov rsi, rdi                ; save
+
+.loop:
+    add r9, 1
+    movzx edi, byte [rsi + r9]  ; read next character
+    test edi, edi
+    je .end                     ; skip the end if '\0'
+
+    call is_alpha_mask          ; 0 if non-alpha, else the mask value
+    test eax, eax
+    je .loop                    ; if 0 then skip
+
+    or r8d, eax                 ; add the mask to total
+    jmp .loop                   ; always loop
+
+.end:
+    xor eax, eax
+    cmp r8d, 0x3ffffff          ; all bits should be set
+    sete al                     ; then set to 1
+    ret
+
+
+is_alpha_mask:
+    ; rdi
+    xor eax, eax
+    or rdi, 32                  ; lowercase
+    sub rdi, 97                 ; subtract 'a'
+    cmp rdi, 26
+    jae .end                    ; return 0 if >= 26
+    mov eax, 1
+    mov rcx, rdi                ; move to cl
+    shl eax, cl                 ; left-shift
+.end:
+    ret
