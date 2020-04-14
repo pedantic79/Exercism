85e28a9ff25dac710da6012128425f78e51267ac
Dan Nguyen <pedantic@gmail.com>
2020-04-13 20:29:41 -0500

    Update typescript/bob based on feedback

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -1,28 +1,30 @@
+// Regex: Matches at least one uppercase
+const ATLEAST_ONE_UPPERCASE = /[A-Z]/;
+
+const isSilence = (msg: string): boolean => msg.trim().length === 0;
+const isQuestion = (msg: string): boolean => msg.trimEnd().endsWith("?");
+const isYell = (msg: string): boolean =>
+  ATLEAST_ONE_UPPERCASE.test(msg) && msg === msg.toUpperCase();
+
 export default class Bob {
   hey(message: string): string {
-    const msg = message.trim();
+    const silence = isSilence(message);
+    const yell = isYell(message);
+    const question = isQuestion(message);
+    const shoutedQuestion = yell && question;
 
-    if (msg === "") {
+    if (silence) {
       return "Fine. Be that way!";
+    } else if (shoutedQuestion) {
+      return "Calm down, I know what I'm doing!";
+    } else if (question) {
+      return "Sure.";
+    } else if (yell) {
+      return "Whoa, chill out!";
     } else {
-      const question = msg.endsWith("?");
-      const yell = isYell(msg);
-
-      if (question) {
-        return yell ? "Calm down, I know what I'm doing!" : "Sure.";
-      } else {
-        return yell ? "Whoa, chill out!" : "Whatever.";
-      }
+      return "Whatever.";
     }
   }
-}
-
-function isYell(msg: string): boolean {
-  // Regex: Matches at least one uppercase, but no lowercase characters
-  const ATLEAST_ONE_UPPERCASE = /^[^a-z]*[A-Z]+[^a-z]*$/;
-  return ATLEAST_ONE_UPPERCASE.test(msg);
-
-  // return msg === msg.toUpperCase() && msg !== msg.toLowerCase();
 }
 
 // This should really go in another file, but I didn't want to bother introducing
@@ -32,4 +34,16 @@ describe("isYell", function () {
   it("no uppercase or lowercase", () => expect(isYell("!@#@!")).toBeFalsy());
   it("one uppercase", () => expect(isYell("!@#@!A")).toBeTruthy());
   it("one lowercase", () => expect(isYell("!@#@!a")).toBeFalsy());
+});
+
+describe("isSilence", function () {
+  it("empty", () => expect(isSilence("")).toBeTruthy());
+  it("only spaces", () => expect(isSilence("    ")).toBeTruthy());
+  it("non-empty, with spaces", () => expect(isSilence("  a    ")).toBeFalsy());
+});
+
+describe("isQuestion", function () {
+  it("empty", () => expect(isQuestion("")).toBeFalsy());
+  it("ends with ?", () => expect(isQuestion("word?")).toBeTruthy());
+  it("? with spaces", () => expect(isQuestion("word?   \t")).toBeTruthy());
 });
