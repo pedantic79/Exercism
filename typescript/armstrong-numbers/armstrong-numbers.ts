2e85b6379aaebc019a071cedf4c864e75be8955e
Dan Nguyen <pedantic@gmail.com>
2020-04-13 20:44:38 -0500

    Update typescript/armstrong-numbers remove ==

--- typescript/armstrong-numbers/armstrong-numbers.ts
+++ typescript/armstrong-numbers/armstrong-numbers.ts
@@ -1,11 +1,14 @@
 class ArmstrongNumbers {
-    static isArmstrongNumber(num: number): boolean {
-        const digits = num.toString().split('')
-        const len = digits.length
-        const armstrong = digits.reduce((acc, digit) => acc + parseInt(digit) ** len, 0)
+  static isArmstrongNumber(num: number): boolean {
+    const digits = num.toString().split("");
+    const len = digits.length;
+    const armstrong = digits.reduce(
+      (acc, digit) => acc + parseInt(digit) ** len,
+      0
+    );
 
-        return armstrong == num
-    }
+    return armstrong === num;
+  }
 }
 
-export default ArmstrongNumbers
+export default ArmstrongNumbers;
