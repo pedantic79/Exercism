0834ff191ca1b17b3ffa0db5e10d402f9a801a27
Dan Nguyen <pedantic@gmail.com>
2020-04-15 00:09:11 -0500

    Update python/acronym based on feedback

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,12 +1,8 @@
 def normalize_input(input_str):
-    for (character, replacement) in [("-", " "), ("_", "")]:
-        input_str = input_str.replace(character, replacement)
-    return input_str
+    return input_str.replace("-", " ").replace("_", "")
 
 
 def abbreviate(words):
     return "".join(
-        word[0].upper()
-        for word in normalize_input(words).split(" ")
-        if word != ""
+        word[0].upper() for word in normalize_input(words).split(" ") if word
     )
