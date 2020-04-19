f9ab268ace171815eb9c45c19023bd4b83c1b5da
Dan Nguyen <pedantic@gmail.com>
2020-04-18 20:52:06 -0500

    Update typescript/robot-name based on feedback

--- typescript/robot-name/robot-name.ts
+++ typescript/robot-name/robot-name.ts
@@ -1,9 +1,49 @@
+const ASCII_A: number = "A".charCodeAt(0);
+const ASCII_0: number = "0".charCodeAt(0);
+
+class RobotFactory {
+  name_cache: Set<string> = new Set();
+
+  generateName(): string {
+    let name = this.randomName();
+    while (this.name_cache.has(name)) {
+      name = this.randomName();
+    }
+
+    this.name_cache.add(name);
+    return name;
+  }
+
+  randomCharacter(start: number, size: number): string {
+    return String.fromCharCode(start + Math.floor(Math.random() * size));
+  }
+
+  randomLetter(): string {
+    return this.randomCharacter(ASCII_A, 26);
+  }
+
+  randomNumber(): string {
+    return this.randomCharacter(ASCII_0, 10);
+  }
+
+  randomName(): string {
+    return (
+      this.randomLetter() +
+      this.randomLetter() +
+      this.randomNumber() +
+      this.randomNumber() +
+      this.randomNumber()
+    );
+  }
+}
+
+const FACTORY = new RobotFactory();
+
 export default class RobotName {
   private _name: string;
-  private static NAME_CACHE: Set<string> = new Set();
 
   constructor() {
-    this._name = RobotName.generateName();
+    this._name = FACTORY.generateName();
   }
 
   get name(): string {
@@ -11,41 +51,6 @@ export default class RobotName {
   }
 
   resetName(): void {
-    this._name = RobotName.generateName();
+    this._name = FACTORY.generateName();
   }
-
-  private static generateName(): string {
-    let name = randomName();
-    while (RobotName.NAME_CACHE.has(name)) {
-      name = randomName();
-    }
-
-    RobotName.NAME_CACHE.add(name);
-    return name;
-  }
-}
-
-const ASCII_A: number = "A".charCodeAt(0);
-const ASCII_0: number = "0".charCodeAt(0);
-
-function randomCharacter(start: number, size: number): string {
-  return String.fromCharCode(start + Math.floor(Math.random() * size));
-}
-
-function randomLetter(): string {
-  return randomCharacter(ASCII_A, 26);
-}
-
-function randomNumber(): string {
-  return randomCharacter(ASCII_0, 10);
-}
-
-function randomName(): string {
-  return (
-    randomLetter() +
-    randomLetter() +
-    randomNumber() +
-    randomNumber() +
-    randomNumber()
-  );
 }
