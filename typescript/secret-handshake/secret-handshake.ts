0b2d450c10871ef6db30274ad34d55fac9239348
Dan Nguyen <pedantic@gmail.com>
2020-04-23 22:52:22 -0500

    Update typescript/secret-handshake

--- typescript/secret-handshake/secret-handshake.ts
+++ typescript/secret-handshake/secret-handshake.ts
@@ -1,20 +1,21 @@
-const SECRET_HANDSHAKE_OPERATIONS = [
-  "wink",
-  "double blink",
-  "close your eyes",
-  "jump",
-];
+const SECRET_HANDSHAKE_OPERATIONS = {
+  0b0001: "wink",
+  0b0010: "double blink",
+  0b0100: "close your eyes",
+  0b1000: "jump",
+};
+
+const REVERSE_HANDSHAKE_KEY = 0b10000;
 
 export default class Handshake {
   constructor(private readonly secret: number) {}
 
   commands(): string[] {
-    const shake = SECRET_HANDSHAKE_OPERATIONS.filter(
-      (_, handshakeIndex) => this.secret & (1 << handshakeIndex)
-    );
+    const shake = Object.entries(SECRET_HANDSHAKE_OPERATIONS)
+      .sort()
+      .filter(([handshakeType, _]) => this.secret & Number(handshakeType))
+      .map(([_, handShakeOperation]) => handShakeOperation);
 
-    return this.secret & (1 << SECRET_HANDSHAKE_OPERATIONS.length)
-      ? shake.reverse()
-      : shake;
+    return this.secret & REVERSE_HANDSHAKE_KEY ? shake.reverse() : shake;
   }
 }
