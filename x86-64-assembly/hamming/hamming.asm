c2a319678c0a16af32f0c0caf8e7e0b4e7f31661
Dan Nguyen <pedantic@gmail.com>
2020-04-26 21:03:29 -0500

    Add x86-64-assembly/hamming

---
+++ x86-64-assembly/hamming/hamming.asm
@@ -0,0 +1,29 @@
+section .text
+global distance
+distance:
+        xor     edx, edx
+        xor     eax, eax
+.start:
+        mov     r8b, byte [rdi + rdx]
+        mov     cl, byte [rsi + rdx]
+
+        ;; checks are not null
+        test    r8b, r8b
+        je      .end_while
+        test    cl, cl
+        je      .end_while
+
+        ;; increment if characters are different
+        cmp     r8b, cl
+        je      .increment_loop
+        add     eax, 1
+.increment_loop:
+        add     rdx, 1
+        jmp     .start
+
+.end_while:
+        cmp     r8b, cl
+        je      .equal_len
+        or      eax, -1
+.equal_len:
+        ret
