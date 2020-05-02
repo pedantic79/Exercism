8082aa54e770f01909a1bca5edf2bb0127c5a574
Dan Nguyen <pedantic@gmail.com>
2020-05-01 22:24:01 -0500

    Update cpp/reverse-string to support unicode

--- cpp/reverse-string/reverse_string.cpp
+++ cpp/reverse-string/reverse_string.cpp
@@ -1,12 +1,28 @@
 #include "reverse_string.h"
+#include "test/catch.hpp"
+#include <algorithm>
+#include <codecvt>
+#include <locale>
+#include <string>
+#include <string_view>
 
-using namespace std;
+std::string to_utf8(std::u32string_view s) {
+    std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> conv;
+    return conv.to_bytes(s.begin(), s.end());
+}
 
-// Using string_view from C++17, if this were to be C++11 compliant I would
-// use a `const string&` instead. This however means that if a `const char *`
-// would create a string object implicitly. The only work around to this would
-// be to have two implementations one that took a `const string&` and one
-// that took a `const char *`
-string reverse_string::reverse_string(string_view s) {
-    return string(rbegin(s), rend(s));
+std::u32string to_utf32(std::string_view s) {
+    std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> conv;
+    return conv.from_bytes(s.begin(), s.end());
+}
+
+std::string reverse_string::reverse_string(std::string_view s) {
+    auto unicode = to_utf32(s);
+
+    std::reverse(unicode.begin(), unicode.end());
+    return to_utf8(unicode);
+}
+
+TEST_CASE("wide_character") {
+    REQUIRE("猫子" == reverse_string::reverse_string("子猫"));
 }
