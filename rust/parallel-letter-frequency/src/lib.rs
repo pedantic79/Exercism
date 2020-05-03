7abcfaf609f799d21d09ef18c9a1bdc718b14a20
Dan Nguyen <pedantic@gmail.com>
2020-05-03 16:50:40 -0500

    Update rust/parallel-letter-frequency add multiple solutions

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,36 +1,136 @@
-// use rayon::iter::ParallelIterator;
-// use rayon::slice::ParallelSlice;
-use crossbeam_utils::thread;
 use std::collections::HashMap;
 
-pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    if input.is_empty() || worker_count == 0 {
-        return HashMap::new();
+#[cfg(feature = "pariter")]
+pub use crate::rayon::frequency;
+
+#[cfg(feature = "crossbeam")]
+pub use crate::crossbeam::frequency;
+
+#[cfg(feature = "default")]
+pub use crate::stdthread::frequency;
+
+fn count<S: AsRef<str>>(texts: &[S]) -> HashMap<char, usize> {
+    let mut map = HashMap::new();
+
+    for line in texts.iter() {
+        for chr in line.as_ref().chars().filter(|c| c.is_alphabetic()) {
+            if let Some(c) = chr.to_lowercase().next() {
+                (*map.entry(c).or_insert(0)) += 1;
+            }
+        }
     }
 
-    let size = (input.len() + worker_count - 1) / worker_count;
+    map
+}
 
-    // override usage of global pool
-    // pool.install(|| {
-    // input
-    //     .par_chunks(size)
-    //     .map(count)
-    //     .reduce(HashMap::new, |mut acc, value| {
-    //         for (ch, count) in value {
-    //             acc.entry(ch).and_modify(|n| *n += count).or_insert(count);
-    //         }
-    //         acc
-    //     })
-    // })
+fn calc_size(len: usize, worker_count: usize) -> usize {
+    (len + worker_count - 1) / worker_count
+}
 
-    if size < 5 {
-        return count(input);
+#[cfg(feature = "pariter")]
+mod rayon {
+    use super::{calc_size, count};
+    use rayon::iter::ParallelIterator;
+    use rayon::slice::ParallelSlice;
+    use std::collections::HashMap;
+
+    #[allow(dead_code)]
+    pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
+        if input.is_empty() || worker_count == 0 {
+            return HashMap::new();
+        }
+
+        let size = calc_size(input.len(), worker_count);
+        if size < 5 {
+            return count(input);
+        }
+
+        // Create a custom sized thread pool for each run
+        let pool = rayon::ThreadPoolBuilder::new()
+            .num_threads(worker_count)
+            .build()
+            .unwrap();
+
+        // override usage of global pool
+        pool.install(|| {
+            input
+                .par_chunks(size)
+                .map(count)
+                .reduce(HashMap::new, |mut acc, value| {
+                    for (ch, count) in value {
+                        acc.entry(ch).and_modify(|n| *n += count).or_insert(count);
+                    }
+                    acc
+                })
+        })
     }
+}
 
-    thread::scope(|scope| {
+#[cfg(feature = "crossbeam")]
+mod crossbeam {
+    use super::{calc_size, count};
+    use crossbeam_utils::thread;
+    use std::collections::HashMap;
+
+    #[allow(dead_code)]
+    pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
+        if input.is_empty() || worker_count == 0 {
+            return HashMap::new();
+        }
+
+        let size = calc_size(input.len(), worker_count);
+        if size < 5 {
+            return count(input);
+        }
+
+        thread::scope(|scope| {
+            let thread_handles = input
+                .chunks(size)
+                .map(|chunk| scope.spawn(move |_| count(chunk)))
+                .collect::<Vec<_>>();
+
+            thread_handles
+                .into_iter()
+                .fold(HashMap::new(), |mut acc, handle| {
+                    let hm = handle.join().unwrap();
+                    for (ch, count) in hm {
+                        acc.entry(ch).and_modify(|n| *n += count).or_insert(count);
+                    }
+                    acc
+                })
+        })
+        .unwrap()
+    }
+}
+
+#[cfg(feature = "default")]
+mod stdthread {
+    use super::{calc_size, count};
+    use std::collections::HashMap;
+    use std::thread;
+
+    #[allow(dead_code)]
+    pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
+        if input.is_empty() || worker_count == 0 {
+            return HashMap::new();
+        }
+
+        let size = calc_size(input.len(), worker_count);
+        if size < 5 {
+            return count(input);
+        }
+
         let thread_handles = input
             .chunks(size)
-            .map(|chunk| scope.spawn(move |_| count(chunk)))
+            .map(|chunk| {
+                let v = chunk
+                    .iter()
+                    .copied()
+                    .map(|s| s.to_string())
+                    .collect::<Vec<_>>();
+
+                thread::spawn(move || count(&v))
+            })
             .collect::<Vec<_>>();
 
         thread_handles
@@ -42,20 +142,5 @@ pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
                 }
                 acc
             })
-    })
-    .unwrap()
-}
-
-fn count(texts: &[&str]) -> HashMap<char, usize> {
-    let mut map = HashMap::new();
-
-    for line in texts {
-        for chr in line.chars().filter(|c| c.is_alphabetic()) {
-            if let Some(c) = chr.to_lowercase().next() {
-                (*map.entry(c).or_insert(0)) += 1;
-            }
-        }
     }
-
-    map
 }
