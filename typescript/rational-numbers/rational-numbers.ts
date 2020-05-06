a174e3b49d65bd8467dcad9925be47e07e668dc9
Dan Nguyen <pedantic@gmail.com>
2020-05-06 17:42:32 -0500

    Add typescript/rational-numbers

---
+++ typescript/rational-numbers/rational-numbers.ts
@@ -0,0 +1,71 @@
+/*
+ * @prettier
+ */
+
+export default class Rational {
+  constructor(private numerator: number, private denominator: number) {
+    this.reduce();
+  }
+
+  reduce(): Rational {
+    const gcd = this.greatestCommonDivisor(this.numerator, this.denominator);
+
+    this.numerator /= gcd;
+    this.denominator /= gcd;
+
+    if (this.denominator < 0) {
+      this.denominator *= -1;
+      this.numerator *= -1;
+    }
+
+    return this;
+  }
+
+  add({ numerator, denominator }: Rational): Rational {
+    return new Rational(
+      numerator * this.denominator + this.numerator * denominator,
+      denominator * this.denominator
+    );
+  }
+
+  sub(rhs: Rational): Rational {
+    rhs.numerator *= -1;
+    return this.add(rhs);
+  }
+
+  mul({ numerator, denominator }: Rational): Rational {
+    return new Rational(
+      this.numerator * numerator,
+      this.denominator * denominator
+    );
+  }
+
+  div({ numerator, denominator }: Rational): Rational {
+    return this.mul(new Rational(denominator, numerator));
+  }
+
+  abs(): Rational {
+    return new Rational(Math.abs(this.numerator), Math.abs(this.denominator));
+  }
+
+  exprational(n: number): Rational {
+    return new Rational(
+      Math.pow(this.numerator, n),
+      Math.pow(this.denominator, n)
+    );
+  }
+
+  expreal(base: number): number {
+    return Number(
+      Math.pow(base, this.numerator / this.denominator).toPrecision(15)
+    );
+  }
+
+  private greatestCommonDivisor(a: number, b: number): number {
+    while (b !== 0) {
+      [a, b] = [b, a % b];
+    }
+
+    return a;
+  }
+}
