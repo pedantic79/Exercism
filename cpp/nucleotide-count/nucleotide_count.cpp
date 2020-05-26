1d01add2bb1d3c78d2c987b405ce2ec869da0c80
Dan Nguyen <pedantic@gmail.com>
2020-05-26 18:47:45 -0500

    Refactor cpp/nucleotide-count

--- cpp/nucleotide-count/nucleotide_count.cpp
+++ cpp/nucleotide-count/nucleotide_count.cpp
@@ -1,1 +1,40 @@
 #include "nucleotide_count.h"
+
+namespace {
+void is_valid(char c) {
+    switch (c) {
+    case 'A':
+    case 'T':
+    case 'C':
+    case 'G':
+        return;
+    }
+
+    throw std::invalid_argument{"invalid nucleotide"};
+}
+
+} // namespace
+
+namespace nucleotide_count {
+
+counter::counter(const std::string &dna_sequence) : s_{dna_sequence} {
+    std::for_each(s_.begin(), s_.end(), ::is_valid);
+}
+
+int counter::count(char nucleotide) const {
+    is_valid(nucleotide);
+
+    return std::count_if(s_.begin(), s_.end(), [nucleotide](const char c) {
+        return c == nucleotide;
+    });
+}
+
+std::map<char, int> counter::nucleotide_counts() const {
+    std::map<char, int> ret{{'A', 0}, {'T', 0}, {'C', 0}, {'G', 0}};
+
+    for (char c : s_) {
+        ret[c]++;
+    }
+    return ret;
+}
+} // namespace nucleotide_count
--- cpp/nucleotide-count/nucleotide_count.h
+++ cpp/nucleotide-count/nucleotide_count.h
@@ -1,46 +1,17 @@
 #ifndef NUCLEOTIDE_COUNT_H
 #define NUCLEOTIDE_COUNT_H
+#include <algorithm>
 #include <map>
 #include <string>
-#include <algorithm>
 
 namespace nucleotide_count {
 class counter {
-    std::string s;
-
-    static void is_valid(char c) {
-        switch (c) {
-        case 'A':
-        case 'T':
-        case 'C':
-        case 'G':
-            return;
-        }
-
-        throw std::invalid_argument{"invalid nucleotide"};
-    }
+    std::string s_;
 
   public:
-    counter(const std::string &dna_sequence) : s{dna_sequence} {
-        std::for_each(std::begin(s), std::end(s), is_valid);
-    }
-
-    int count(char nucleotide) const {
-        is_valid(nucleotide);
-
-        return std::count_if(
-            std::begin(s), std::end(s),
-            [nucleotide](const char c) { return c == nucleotide; });
-    }
-
-    std::map<char, int> nucleotide_counts() const {
-        std::map<char, int> ret{{'A', 0}, {'T', 0}, {'C', 0}, {'G', 0}};
-
-        for (char c : s) {
-            ret[c]++;
-        }
-        return ret;
-    }
+    explicit counter(const std::string &dna_sequence);
+    int count(char nucleotide) const;
+    std::map<char, int> nucleotide_counts() const;
 };
 
 } // namespace nucleotide_count
