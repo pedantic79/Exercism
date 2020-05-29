ea9301a49bc6c44bdd9da3497f09a14cd8fface8
Dan Nguyen <pedantic@gmail.com>
2020-05-29 15:50:46 -0500

    Update python/grade-school

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -4,18 +4,18 @@ from collections import defaultdict
 
 class School:
     def __init__(self) -> None:
-        self.student_roster: Dict[int, List[str]] = defaultdict(list)
+        self._student_roster: Dict[int, List[str]] = defaultdict(list)
 
     def add_student(self, name: str, grade: int) -> None:
-        self.student_roster[grade].append(name)
-        self.student_roster[grade].sort()
+        self._student_roster[grade].append(name)
+        self._student_roster[grade].sort()
 
     def roster(self) -> List[str]:
         return [
             name
-            for grade in sorted(self.student_roster.keys())
-            for name in self.student_roster[grade]
+            for grade in sorted(self._student_roster.keys())
+            for name in self.grade(grade)
         ]
 
     def grade(self, grade_number: int) -> List[str]:
-        return self.student_roster[grade_number]
+        return self._student_roster[grade_number]
