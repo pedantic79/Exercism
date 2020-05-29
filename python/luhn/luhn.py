30f1dd74a425029ef0a0162daf6d9ab3030b53d4
Dan Nguyen <pedantic@gmail.com>
2020-05-29 16:20:22 -0500

    Add python/luhn

---
+++ python/luhn/luhn.py
@@ -0,0 +1,26 @@
+class Luhn:
+    def __init__(self, card_num: str):
+        self._card_num = list(reversed(card_num.replace(" ", "")))
+
+    def valid(self) -> bool:
+        try:
+            total = sum(
+                double_if_second(index, character)
+                for (index, character) in enumerate(self._card_num)
+            )
+        except ValueError:
+            return False
+
+        return len(self._card_num) > 1 and total % 10 == 0
+
+
+def double_if_second(index: int, character: str) -> int:
+    number = int(character)
+
+    if index % 2 == 1:
+        if number < 5:
+            number *= 2
+        else:
+            number = number * 2 - 9
+
+    return number
