3566d496bfd6bb62f61773319fb23f64dc901f8e
Dan Nguyen <pedantic@gmail.com>
2020-06-06 17:28:42 -0500

    Update cpp/pangram; remove calls to cctype functions

--- cpp/pangram/pangram.cpp
+++ cpp/pangram/pangram.cpp
@@ -1,21 +1,16 @@
 #include "pangram.h"
-
-#include <algorithm>
 #include <bitset>
-#include <cctype>
 #include <string>
-
-const std::string alpha = "QWERTYUIOPASDFGHJKLZXCVBNM";
 
 bool pangram::is_pangram(const std::string &str) {
     std::bitset<26> letters;
 
-    std::for_each(str.cbegin(), str.cend(), [&](auto c) {
-        if (std::isalpha(c)) {
-            const auto pos = std::tolower(c) - 'a';
+    for (auto c : str) {
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+            const auto pos = (c | 0x20) - 'a';
             letters.set(pos);
         }
-    });
+    }
 
     return letters.all();
 }
