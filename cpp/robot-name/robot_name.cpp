b335cf7fe8b50c6e9db9111eb55b2c7dbf979be1
Dan Nguyen <pedantic@gmail.com>
2020-06-11 18:04:30 -0500

    Update cpp/robot-name; refactor to make generateName

--- cpp/robot-name/robot_name.cpp
+++ cpp/robot-name/robot_name.cpp
@@ -3,35 +3,16 @@
 #include <string>
 #include <unordered_map>
 
-namespace {
-std::string generateRandomName() {
-    std::string name;
-    name.reserve(5);
-
-    thread_local std::random_device rd;
-    thread_local std::mt19937 random_generator{rd()};
-
-    std::uniform_int_distribution<char> random_ascii_letter{'A', 'Z'};
-    std::uniform_int_distribution<char> random_ascii_number{'0', '9'};
-
-    for (int i = 0; i < 2; ++i) {
-        name += random_ascii_letter(random_generator);
-    }
-    for (int i = 0; i < 3; ++i) {
-        name += random_ascii_number(random_generator);
-    }
-
-    return name;
-}
-} // namespace
-
 std::unordered_set<std::string> robot_name::robot::namelist_;
 
-robot_name::robot::robot() { generateName(); }
+robot_name::robot::robot() {
+    name_.reserve(5);
+    generateName();
+}
 
 void robot_name::robot::generateName() {
     do {
-        name_ = generateRandomName();
+        generateRandomName();
     } while (namelist_.find(name_) != namelist_.end());
 
     namelist_.insert(name_);
@@ -40,3 +21,19 @@ void robot_name::robot::generateName() {
 void robot_name::robot::reset() { generateName(); }
 
 const std::string robot_name::robot::name() const { return name_; }
+
+void robot_name::robot::generateRandomName() {
+    thread_local std::random_device rd;
+    thread_local std::mt19937 random_generator{rd()};
+
+    std::uniform_int_distribution<char> random_ascii_letter{'A', 'Z'};
+    std::uniform_int_distribution<char> random_ascii_number{'0', '9'};
+
+    name_.erase();
+    for (int i = 0; i < 2; ++i) {
+        name_ += random_ascii_letter(random_generator);
+    }
+    for (int i = 0; i < 3; ++i) {
+        name_ += random_ascii_number(random_generator);
+    }
+}
