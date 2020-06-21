af2039680dcc63e0005491f67d6c9cc4fc08cd5b
Dan Nguyen <pedantic@gmail.com>
2020-06-21 00:46:09 -0500

    Add cpp/difference-of-squares

---
+++ cpp/difference-of-squares/difference_of_squares.h
@@ -0,0 +1,11 @@
+#if !defined(DIFFERENCE_OF_SQUARES_H)
+#define DIFFERENCE_OF_SQUARES_H
+
+namespace difference_of_squares {
+unsigned square_of_sum(unsigned);
+unsigned sum_of_squares(unsigned);
+unsigned difference(unsigned);
+
+} // namespace difference_of_squares
+
+#endif // DIFFERENCE_OF_SQUARES_H
