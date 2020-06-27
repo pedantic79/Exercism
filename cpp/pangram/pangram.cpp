7cee799774c29b8fd0308a6929ed33e75cc38c48
Dan Nguyen <pedantic@gmail.com>
2020-06-26 21:01:56 -0500

    Update cpp/pangram; use locale

--- cpp/pangram/pangram.cpp
+++ cpp/pangram/pangram.cpp
@@ -1,13 +1,14 @@
 #include "pangram.h"
 #include <bitset>
+#include <locale>
 #include <string>
 
 bool pangram::is_pangram(const std::string &str) {
     std::bitset<26> letters;
 
     for (auto c : str) {
-        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
-            const auto pos = (c | 0x20) - 'a';
+        if (std::isalpha(c, std::locale::classic())) {
+            const auto pos = std::tolower(c, std::locale::classic()) - 'a';
             letters.set(pos);
         }
     }
