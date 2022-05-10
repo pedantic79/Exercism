f45f385e1b4802105d6a119ff9331d28ca8e22ff
Dan Nguyen <pedantic@gmail.com>
2022-05-10 17:39:57 -0500

    Update rust/palindrome-products

--- rust/palindrome-products/src/lib.rs
+++ rust/palindrome-products/src/lib.rs
@@ -1,32 +1,52 @@
-pub mod products;
-
-use products::palindrome_products_mutex;
-use std::collections::BTreeSet;
-
-#[derive(Debug, PartialEq, Eq, Clone)]
-pub struct Palindrome(BTreeSet<(u64, u64)>, u64);
+/// `Palindrome` is a newtype which only exists when the contained value is a palindrome number in base ten.
+///
+/// A struct with a single field which is used to constrain behavior like this is called a "newtype", and its use is
+/// often referred to as the "newtype pattern". This is a fairly common pattern in Rust.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Palindrome(u64);
 
 impl Palindrome {
-    pub fn new(a: u64, b: u64) -> Self {
-        Self(std::iter::once((a, b)).collect(), a * b)
-    }
-
-    pub fn value(&self) -> u64 {
-        self.1
-    }
-
-    pub fn insert(&mut self, a: u64, b: u64) {
-        self.0.insert((a, b));
-    }
-
-    fn insert_palindrome(&mut self, other: &Self) {
-        for (k, v) in other.0.iter() {
-            self.insert(*k, *v)
+    /// Create a `Palindrome` only if `value` is in fact a palindrome when represented in base ten. Otherwise, `None`.
+    pub fn new(value: u64) -> Option<Palindrome> {
+        if is_palindrome(value) {
+            Some(Palindrome(value))
+        } else {
+            None
         }
+    }
+
+    /// Get the value of this palindrome.
+    pub fn into_inner(self) -> u64 {
+        self.0
     }
 }
 
-#[inline(always)]
 pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome)> {
-    palindrome_products_mutex(min, max)
+    let mut mn = None;
+    let mut mx = None;
+
+    for i in min..=max {
+        for j in i..=max {
+            let mult = Some(i * j);
+
+            if mult < mn || mult > mx && is_palindrome(mult.unwrap()) {
+                mn = mn.min(mult).or(mult);
+                mx = mx.max(mult).or(mult);
+            }
+        }
+    }
+
+    mn.map(Palindrome).zip(mx.map(Palindrome))
+}
+
+fn is_palindrome(num: u64) -> bool {
+    let s = num.to_string();
+    let bytes = s.as_bytes();
+    let l = bytes.len();
+    for idx in 0..l / 2 {
+        if bytes[idx] != bytes[l - 1 - idx] {
+            return false;
+        }
+    }
+    true
 }
--- rust/palindrome-products/src/main.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    let f = palindrome_products::palindrome_products(1000, 9999);
-    println!("{:?}", f);
-}
